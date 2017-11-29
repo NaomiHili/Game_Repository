0135c9a2bd7ac74496ab8b50f5cfbd6d04f46551
naomi hili <naomi hili>
2017-11-29 17:41:19 +0100

    - started the game - created all needed files - started the start scene - made start and quit buttons - linked the start button with level_1 - started level 1

---
+++ HomeAssignment_game/Home_assignment/Assets/Script/Paddel_1.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddel_1 : MonoBehaviour {
+    float x = 0f;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+   
+        if (Input.GetKey(KeyCode.RightArrow))
+        { x += .2f; }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        { x -= .2f; }
+
+        this.transform.position = new Vector3(x, transform.position.y, transform.position.z);
+    }
+}
---
+++ HomeAssignment_game/Home_assignment/Assets/Script/paddle_2.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class paddle_2 : MonoBehaviour {
+    float x = 0f;
+    // Use this for initialization
+    void Start () {
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        float mousePosInUnits = (Input.mousePosition.x / Screen.width * 16) - 8;
+
+        Vector3 newPaddlePos = new Vector3(mousePosInUnits, gameObject.transform.position.y,
+                                            gameObject.transform.position.z);
+
+        // we needed to create a new variable for the new paddle position since we cannot change
+        // one coordinate (e.g. x coordinate) on its own
+
+        newPaddlePos.x = Mathf.Clamp(mousePosInUnits, -7.35f, 7.35f);
+
+        gameObject.transform.position = newPaddlePos;
+
+    }
+}
